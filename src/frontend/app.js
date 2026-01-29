d8670deedd56b1e993f40db6c90fec5bc0f7484a
limat-victor-emf <victor.limat@studentfr.ch>
2026-01-29 11:32:37 +0100

    p

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "http://52.146.144.224:8080/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
@@ -32,22 +32,22 @@ $(document).ready(function () {
     const $taskElement = $(this).closest("li"); // Trouve l'élément li parent
     const taskId = $taskElement.data("id");
     const isCompleted = $taskElement.hasClass("completed");
-  
+
     // Récupère le texte directement comme un nœud
     const description = $taskElement.contents().filter(function () {
       return this.nodeType === 3; // Node type 3 = texte
     }).text().trim();
-  
+
     console.log("Description trouvée :", description);
-  
+
     if (!description) {
       console.error("Erreur : la description de la tâche est vide !");
       return;
     }
-  
+
     // Prépare l'objet mis à jour
     const updatedTask = { id: taskId, description: description, completed: !isCompleted };
-  
+
     try {
       await fetch(apiEndpoint, {
         method: "PUT",
